878bdaf7e7ae2bceb766a04abb4ac6fdd6631f39
The Realist <nitin.armstrong@gmail.com>
2019-10-30 14:04:47 -0400

    Formatting and updated work 10/30

--- canvas_analyzer.py
+++ canvas_analyzer.py
@@ -18,16 +18,33 @@ __version__ = 7
 
 # 1) main
 def main(user: str):
+    '''
+
+    :param user:
+    :return:
+    '''
 
 
 # 2) print_user_info
 def print_user_info(dicty: dict):
-    for str in dicty:
-        print(str)
+    '''
+
+    :param dicty:
+    :return:
+    '''
+    print("Name: " + dicty["name"])
+    print("Title: " + dicty["title"])
+    print("Email: " + dicty["primary_email"])
+    print("Bio: " + dicty["bio"])
 
 
 # 3) filter_available_courses
-def filter_available_courses(courses: list) -> list:
+def filter_available_courses(courses: [list]) -> [list]:
+    '''
+
+    :param courses:
+    :return:
+    '''
     coursey = []
     for available in courses:
         if available["workflow_state"] == 'available':
@@ -38,20 +55,43 @@ def filter_available_courses(courses: list) -> list:
 
 
 # 4) print_courses
-def print_courses(courses:list):
+def print_courses(courses: list):
+    '''
+
+    :param courses:
+    :return:
+    '''
     for info in courses:
-        print(info["course_id"])
-        print(info[""]) #yo chief find out the key
+        print("ID: " + info["course_id"])
+        print("Name " + info["name"])
 
 
 # 5) get_course_ids
-def get_course_ids(courses:list)->[int]:
-    id=[]
+def get_course_ids(courses: list) -> [int]:
+    '''
+
+    :param courses:
+    :return:
+    '''
+    id = []
     for info in courses:
         id.append(info["course_id"])
     return id
+
+
 # 6) choose_course
-def choose_course(listy:[int])->int
+def choose_course(listy: [int]) -> int:
+    '''
+
+    :param listy:
+    :return:
+    '''
+    choosy = int(input("Enter a valid course ID:"))
+    while choosy not in listy:
+        choosy = int(input("Enter a valid course ID:"))
+    return choosy
+
+
 # 7) summarize_points
 # 8) summarize_groups
 # 9) plot_scores
